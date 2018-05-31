9ec27bfc8a71e109e7e6fee5b913f8f069bd83c8
Dan <dpedersen84@gmail.com>
2018-05-31 14:17:18 -0500

    un did some commented out code

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -8,9 +8,9 @@ module.exports = function(app, passport) {
         res.sendFile(path.join(__dirname, "../public/index.html"));
     })
 
-    // app.get("/user", function(req, res) {
-        // res.sendFile(path.join(__dirname, "../public/main.html"));
-    // })
+    app.get("/user", function(req, res) {
+        res.sendFile(path.join(__dirname, "../public/main.html"));
+    })
 
     app.get("/trips", function(req, res) {
         res.sendFile(path.join(__dirname, "../public/trips.html"));
