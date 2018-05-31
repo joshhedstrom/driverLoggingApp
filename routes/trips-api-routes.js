6a5a555b0e1cea92853724ace574f1caa027a7ea
BillCarverDev <carverwr@gmail.com>
2018-05-30 20:34:14 -0500

    Added delete code in trips route

--- routes/trips-api-routes.js
+++ routes/trips-api-routes.js
@@ -5,12 +5,34 @@ const db = require("../models");
 module.exports = function(app) {
 
     // All trips
+    // app.get("/api/trips/", function(req, res) {
+    //     db.Trips.findAll({})
+    //         .then(function(dbTrips) {
+    //             console.log(dbTrips);
+    //             res.json(dbTrips);
+    //         })
+    // })
+
+
+
+
+
+
+
+
+
+    // Delete a Trip
     app.get("/api/trips/", function(req, res) {
-        db.Trips.findAll({})
-            .then(function(dbTrips) {
+        
+        console.log(req.body);
+        db.Trips.destroy(req.body).then(function(dbTrips) {
+            where: {
+                id: req.params.id
+            }
+            res.json(dbTrips)
                 console.log(dbTrips);
-                res.json(dbTrips);
-            })
+            });
+            
     })
 
 }
