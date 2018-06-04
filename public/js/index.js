856df09b968cb199b29ec4e25c79ecc21b7317c4
Josh Hedstrom <josh.hedstrom@gmail.com>
2018-06-04 11:07:52 -0500

    changed form, sets ID in session storage

--- public/js/index.js
+++ public/js/index.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 
-    //LOG IN------------------------------------------------------------------------>>
+    //SIGN UP----------------------------------------------------------------------->>
 
     $('#new-user-submit').click(function(e) {
         e.preventDefault()
@@ -10,21 +10,43 @@ $(document).ready(function() {
             let password = $('#new-user-password').val().trim();
 
             let user = {
-            	username: username,
-            	password: password
+                username: username,
+                password: password
             }
 
-            $.post("/signup", user, function() {
+            // $.post("/signup", user, function() {
 
-            })
+            // })
         });
+    });
+
+    //LOG IN------------------------------------------------------------------------>>
+
+    $('#returning-user-submit').click(function(e) {
+        e.preventDefault()
+
+        // $.post("/login", function(data) {
+            let username = $('#returning-user-username').val().trim();
+            let password = $('#returning-user-password').val().trim();
+
+            let user = {
+                username: username,
+                password: password
+            }
+
+            $.post("/login", user, function(data) {
+                sessionStorage.setItem('userID', data.id)
+                //THEN REDIRECT TO /user
+            })
+        // });
     });
 
 
 
-        $.get("/user", function(data) {
-            
-        });
-    
+
+    // $.get("/user", function(data) {
+
+    // });
+
 
 });
